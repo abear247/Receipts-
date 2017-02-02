46963e0e1b44ab304e0062c9c804f27fe997fb77
alexbearinger <alexbearinger@Alexs-MacBook-Pro.local>
2017-02-02 11:28:07 -0500

    first commit

---
+++ Receipts++/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Receipts++
+//
+//  Created by Alex Bearinger on 2017-02-02.
+//  Copyright © 2017 Alex Bearinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
